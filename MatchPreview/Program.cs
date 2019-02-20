1a3f2c9c9d4aedba39770f4462da2a94666b268e
조호연 <dkdkfjgh@naver.com>
2019-02-21 05:58:47 +0900

    초기 커밋.

---
+++ MatchPreview/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Heroes.ReplayParser;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Net;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace MatchPreview
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HotsMatchPreview hOTSAnalyzer = new HotsMatchPreview();
+            HotsLogsParser hotsLogsParser = new HotsLogsParser();
+
+            hotsLogsParser.HotsLogsParse();
+            while (true)
+            {
+                // hOTSAnalyzer.HotsMatchPreviewAnalyzer();
+            }
+
+
+
+        }
+
+
+    }
+
+    class HotsMatchPreview
+    {
+        static string TemporaryBattlelobbyFileLocation;
+        float currentDpi;
+        DateTime recentBattlelobbyLastWriteTime;
+        string content;
+        private static float GetCurrentDpi()
+        {
+            float result;
+            using (RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\FontDPI"))
+            {
+                if (registryKey != null)
+                {
+                    result = (float)((int)registryKey.GetValue("LogPixels"));
+                }
+                else
+                {
+                    result = 96f;
+                }
+            }
+            return result;
+        }
+        private static Tuple<int, byte[]> BitmapToBytes(Bitmap bitmap)
+        {
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(default(System.Drawing.Point), bitmap.Size), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            byte[] array;
+            try
+            {
+                array = new byte[bitmapData.Stride * bitmap.Height];
+                Marshal.Copy(bitmapData.Scan0, array, 0, array.Length);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+            return new Tuple<int, byte[]>(bitmapData.Stride, array);
+        }
+        private static void BytesToBitmap(Bitmap bitmap, byte[] bytes)
+        {
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(default(System.Drawing.Point), bitmap.Size), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            try
+            {
+                Marshal.Copy(bytes, 0, bitmapData.Scan0, bytes.Length);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
+
+        private string MatchURL = "";
+
+        public void HotsMatchPreviewAnalyzer()
+        {
+            Thread.Sleep(1000);
+            try
+            {
+                TemporaryBattlelobbyFileLocation = Path.GetTempPath() + "\\Heroes of the Storm\\TempWriteReplayP1\\replay.server.BATTLELOBBY";
+                currentDpi = GetCurrentDpi();
+                recentBattlelobbyLastWriteTime = File.GetLastWriteTimeUtc(TemporaryBattlelobbyFileLocation);
+                content = StandaloneBattleLobbyParser.Base64EncodeStandaloneBattlelobby(StandaloneBattleLobbyParser.Parse(File.ReadAllBytes(TemporaryBattlelobbyFileLocation)));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File Not Found... Maybe not in Lobby");
+                return;
+            }
+
+            Console.WriteLine("File Found...! Trying To Parse URL");
+
+            using (Bitmap bitmap = new Bitmap((int)((float)Screen.PrimaryScreen.Bounds.Width * (currentDpi / 96f)), (int)((float)Screen.PrimaryScreen.Bounds.Height * (currentDpi / 96f))))
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, bitmap.Size);
+                }
+                Tuple<int, byte[]> tuple = BitmapToBytes(bitmap);
+                int item = tuple.Item1;
+                byte[] item2 = tuple.Item2;
+                for (int k = 0; k < bitmap.Width; k++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        int num = j * item + k * 3;
+                        decimal d = k / bitmap.Width;
+                        bool flag = d < 0.25m || d > 0.75m;
+                        if (flag && item2[num] == 255 && item2[num + 1] == 255 && item2[num + 2] == 255)
+                        {
+                            item2[num] = (item2[num + 1] = (item2[num + 2] = 0));
+                        }
+                        else if (flag && item2[num] >= 90 && item2[num] <= 160 && item2[num + 1] >= 240 && item2[num + 2] >= 155 && item2[num + 2] <= 200)
+                        {
+                            item2[num] = (item2[num + 1] = (item2[num + 2] = 1));
+                        }
+                        else if (flag && item2[num] == 255 && item2[num + 1] >= 138 && item2[num + 1] <= 165 && item2[num + 2] >= 54 && item2[num + 2] <= 99)
+                        {
+                            item2[num] = (item2[num + 1] = (item2[num + 2] = 2));
+                        }
+                        else if (flag && item2[num] >= 99 && item2[num] <= 132 && item2[num + 1] >= 58 && item2[num + 1] <= 100 && item2[num + 2] == 255)
+                        {
+                            item2[num] = (item2[num + 1] = (item2[num + 2] = 3));
+                        }
+                        else
+                        {
+                            item2[num] = (item2[num + 1] = (item2[num + 2] = byte.MaxValue));
+                        }
+                    }
+                }
+                BytesToBitmap(bitmap, item2);
+                string text = string.Empty;
+                try
+                {
+                    using (HttpClient httpClient = new HttpClient())
+                    {
+                        MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+                        multipartFormDataContent.Add(new StringContent("4"), "Version");
+                        multipartFormDataContent.Add(new StringContent(content), "Data");
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            bitmap.Save(memoryStream, ImageFormat.Png);
+                            bitmap.Save("ScreenShot.png", ImageFormat.Png);
+                            multipartFormDataContent.Add(new ByteArrayContent(memoryStream.ToArray()), "ScreenShot", "ScreenShot.png");
+                        }
+                        using (HttpResponseMessage result = httpClient.PostAsync("https://www.hotslogs.com/Player/MatchPreview", multipartFormDataContent).Result)
+                        {
+                            text = result.Content.ReadAsStringAsync().Result;
+                        }
+                    }
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Error!");
+                    return;
+                }
+                Dictionary<string, string> dictionary = (from i in text.Split(new char[] { '\r' })
+                                                         select i.Split(new char[] { ':' }) into i
+                                                         where i.Length == 2
+                                                         select i).ToDictionary((string[] i) => i[0], (string[] i) => i[1]);
+                if (dictionary.ContainsKey("Status") && dictionary["Status"] == "Success" && dictionary.ContainsKey("MatchID"))
+                {
+                    Console.WriteLine("https://www.hotslogs.com/Player/MatchPreview?MatchID=" + dictionary["MatchID"]);
+                    MatchURL = "https://www.hotslogs.com/Player/MatchPreview?MatchID=" + dictionary["MatchID"];
+                    return;
+                }
+            }
+        }
+
+    }
+    class HotsLogsParser
+    {
+        public List<Player> Players = new List<Player>();
+
+        public void HotsLogsParse()
+        {
+            HtmlAgilityPack.HtmlDocument Hdocument;
+            HtmlWeb Hweb = new HtmlWeb();
+            Hdocument = Hweb.Load("https://www.hotslogs.com/Player/MatchPreview?MatchID=ce4c1345");
+
+
+
+            string NickName;
+            string PlayerID;
+
+            string TLTier;
+            string TLMMR;
+            string Team;
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    NickName = Hdocument.DocumentNode.SelectSingleNode(string.Format("//*[@id=\"ctl00_MainContent_RadGridMatchPreview_ctl00__{0}\"]/td[3]/a", i)).InnerText;
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+
+                Team = Hdocument.DocumentNode.SelectSingleNode(string.Format("//*[@id=\"ctl00_MainContent_RadGridMatchPreview_ctl00__{0}\"]/td[2]", i)).InnerText;
+
+                string tmp = Hdocument.DocumentNode.SelectSingleNode(string.Format("//*[@id=\"ctl00_MainContent_RadGridMatchPreview_ctl00__{0}\"]/td[3]/a", i)).OuterHtml;
+                tmp = tmp.Substring(tmp.IndexOf("PlayerID="));
+                PlayerID = tmp.Substring(9, tmp.IndexOf("\"") - 9);
+
+                WebClient client = new WebClient();
+                client.Encoding = Encoding.UTF8;
+                string json = client.DownloadString("https://api.hotslogs.com/Public/Players/" + PlayerID);
+                try
+                {
+                    json = json.Substring(json.IndexOf("TeamLeague"));
+                    json = json.Substring(0, json.IndexOf("}"));
+
+                    TLTier = json.Substring(json.IndexOf("LeagueID") + 10, json.IndexOf(",") - 10);
+                    TLMMR = json.Substring(json.IndexOf("CurrentMMR") + 12);
+                }
+                catch (Exception)
+                {
+                    TLTier = "?";
+                    TLMMR = "?";
+                }
+
+
+                Players.Add(new Player(NickName, PlayerID, TLTier, TLMMR, Team));
+
+            }
+
+        }
+    }
+    struct Player
+    {
+        string NickName;
+        string PlayerID;
+        string TLTier;
+        string TLMMR;
+        string Team;
+        string ProfileURL;
+        string TierURL;
+        public Player(string NN, string PID, string TLT, string TLMMR, string Team)
+        {
+            NickName = NN;
+            PlayerID = PID;
+            TLTier = TLT;
+            this.TLMMR = TLMMR;
+            this.Team = Team;
+            this.ProfileURL = "https://www.hotslogs.com/Images/PlayerProfileImage/" + PlayerID + ".jpeg";
+            this.TierURL = "https://d1i1jxrdh2kvwy.cloudfront.net/Images/Leagues/" + TLTier + ".png";
+        }
+    }
+}
+
---
+++ MatchPreviewApp/MainWindow.xaml.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MatchPreviewApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HotsMatchPreview hotsMatchPreview = new HotsMatchPreview();
+        DispatcherTimer hotsDetectTimer = new DispatcherTimer();
+        string Match = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            hotsDetectTimer.Interval = TimeSpan.FromSeconds(7);
+            hotsDetectTimer.Tick += new EventHandler(hotsDetect_Tick);
+            hotsDetectTimer.Start();
+
+        }
+
+        private void hotsDetect_Tick(object sender,  EventArgs e)
+        {
+            Match = hotsMatchPreview.HotsMatchPreviewAnalyzer();
+            if (Match.StartsWith("http"))
+            {
+                ParseHotsLogsData(Match);
+                hotsDetectTimer.Stop();
+            }
+            else
+            {
+                try
+                {
+                    GridRemover(PlayerBox01);
+                    GridRemover(PlayerBox02);
+                    GridRemover(PlayerBox03);
+                    GridRemover(PlayerBox04);
+                    GridRemover(PlayerBox05);
+                    GridRemover(PlayerBox06);
+                    GridRemover(PlayerBox07);
+                    GridRemover(PlayerBox08);
+                    GridRemover(PlayerBox09);
+                    GridRemover(PlayerBox10);
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+        }
+
+
+        void ParseHotsLogsData(string MatchUrl)
+        {
+            HotsLogsParser hParser = new HotsLogsParser();
+            hParser.HotsLogsParse(MatchUrl);
+            try
+            {
+                CreatePlayerBox(PlayerBox01, hParser.BlueTeam[0]);
+                CreatePlayerBox(PlayerBox02, hParser.BlueTeam[1]);
+                CreatePlayerBox(PlayerBox03, hParser.BlueTeam[2]);
+                CreatePlayerBox(PlayerBox04, hParser.BlueTeam[3]);
+                CreatePlayerBox(PlayerBox05, hParser.BlueTeam[4]);
+
+                CreatePlayerBox(PlayerBox06, hParser.RedTeam[0]);
+                CreatePlayerBox(PlayerBox07, hParser.RedTeam[1]);
+                CreatePlayerBox(PlayerBox08, hParser.RedTeam[2]);
+                CreatePlayerBox(PlayerBox09, hParser.RedTeam[3]);
+                CreatePlayerBox(PlayerBox10, hParser.RedTeam[4]);
+            }
+            catch (Exception e)
+            {
+
+            }
+            hParser = null;
+            System.GC.Collect(0, GCCollectionMode.Forced);
+            System.GC.WaitForFullGCComplete();
+
+
+        }
+
+        void GridRemover(Grid grid)
+        {
+            grid.Children.Clear();
+        }
+
+        void CreatePlayerBox(Grid grid, Player P)
+        {
+            string NickName, MMR, TierURL, ProfileURL;
+            NickName = P.NickName;
+            MMR = P.TLMMR;
+            TierURL = P.TierURL;
+            ProfileURL = P.ProfileURL;
+
+            Label LNickName = new Label
+            {
+                Content = NickName,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Height = 36,
+                Width = 156,
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            };
+
+            Label LMMR = new Label
+            {
+                Content = "MMR : " + MMR,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Height = 40,
+                Width = 76,
+                Margin = new Thickness(0, 36, 0, 0)
+            };
+
+            BitmapImage most1 = new BitmapImage();
+            Bitmap Bmost1 = null;
+            BitmapImage most2 = new BitmapImage();
+            Bitmap Bmost2 = null;
+            BitmapImage most3 = new BitmapImage();
+            Bitmap Bmost3 = null;
+
+
+            BitmapImage TierBitmap = new BitmapImage();
+            TierBitmap.BeginInit();
+            TierBitmap.UriSource = new Uri(TierURL, UriKind.Absolute);
+            TierBitmap.EndInit();
+            System.Windows.Controls.Image TierImage = new System.Windows.Controls.Image
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Height = 36,
+                Width = 40,
+                Margin = new Thickness(156, 0, 0, 0),
+                Source = TierBitmap
+            };
+
+            try
+            {
+                Bitmap ProfileImage = DownloadBitmap(ProfileURL);
+                Bmost1 = (Bitmap)cropImage(ProfileImage, new System.Drawing.Rectangle(new System.Drawing.Point(20, 46), new System.Drawing.Size(60, 60)));
+                most1 = BitmapToImageSource(Bmost1);
+                Bmost2 = (Bitmap)cropImage(ProfileImage, new System.Drawing.Rectangle(new System.Drawing.Point(90, 46), new System.Drawing.Size(60, 60)));
+                most2 = BitmapToImageSource(Bmost2);
+                Bmost3 = (Bitmap)cropImage(ProfileImage, new System.Drawing.Rectangle(new System.Drawing.Point(160, 46), new System.Drawing.Size(60, 60)));
+                most3 = BitmapToImageSource(Bmost3);
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+            System.Windows.Controls.Image MostImage1 = new System.Windows.Controls.Image
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Height = 40,
+                Width = 40,
+                Margin = new Thickness(76, 36, 0, 0),
+                Source = most1
+            };
+
+            System.Windows.Controls.Image MostImage2 = new System.Windows.Controls.Image
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Height = 40,
+                Width = 40,
+                Margin = new Thickness(121, 36, 0, 0),
+                Source = most2
+            };
+
+            System.Windows.Controls.Image MostImage3 = new System.Windows.Controls.Image
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Height = 40,
+                Width = 40,
+                Margin = new Thickness(166, 36, 0, 0),
+                Source = most3
+            };
+
+            grid.Children.Add(LNickName);
+            grid.Children.Add(LMMR);
+            grid.Children.Add(TierImage);
+            grid.Children.Add(MostImage1);
+            grid.Children.Add(MostImage2);
+            grid.Children.Add(MostImage3);
+
+            Bmost1.Dispose();
+            Bmost2.Dispose();
+            Bmost3.Dispose();
+            most1 = null;
+            most2 = null;
+            most3 = null;
+
+            System.GC.Collect(0, GCCollectionMode.Forced);
+            System.GC.WaitForFullGCComplete();
+
+        }
+        private Bitmap DownloadBitmap(string url)
+        {
+            System.Net.WebRequest request = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream =
+                response.GetResponseStream();
+            return new Bitmap(responseStream);
+        }
+        private static System.Drawing.Image cropImage(System.Drawing.Image img, System.Drawing.Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GridRemover(PlayerBox01);
+            GridRemover(PlayerBox02);
+            GridRemover(PlayerBox03);
+            GridRemover(PlayerBox04);
+            GridRemover(PlayerBox05);
+            GridRemover(PlayerBox06);
+            GridRemover(PlayerBox07);
+            GridRemover(PlayerBox08);
+            GridRemover(PlayerBox09);
+            GridRemover(PlayerBox10);
+            hotsDetectTimer.Start();
+        }
+    }
+}
